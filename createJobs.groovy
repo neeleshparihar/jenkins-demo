8cac7d39e25c818eeb986b694a47d2942a1e727e
neeleshparihar <pariharneelesh@gmail.com>
2021-03-21 09:00:57 -0400

    Initial checking

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,22 @@
-pipelineJob('pipelineJob') {
+/*pipelineJob('pipelineJob') {
     definition {
         cps {
             script(readFileFromWorkspace('pipelineJob.groovy'))
             sandbox()
+        }
+    }
+}*/
+pipelineJob('enrollment-service-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/neeleshparihar/enrollment-service.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
